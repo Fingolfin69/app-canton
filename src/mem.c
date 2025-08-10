59062563a5e3a04cf7efbb47eda7732b5aac78ab
Roman Maslennikov <kafkapro@gmail.com>
2025-08-10 23:16:42 +0300

    Enable PB dynamic memory allocator

--- src/mem.c
+++ src/mem.c
@@ -7,14 +7,24 @@
  */
 
 #include <stdint.h>
+#include <string.h>
 #include "mem.h"
 #include "mem_alloc.h"
 #include "os_print.h"
+#include "ledger_assert.h"
 
 #define SIZE_MEM_BUFFER (1024 * 16)
 
 static uint8_t mem_buffer[SIZE_MEM_BUFFER] __attribute__((aligned(sizeof(intmax_t))));
 static mem_ctx_t mem_ctx = NULL;
+
+typedef struct {
+    void *ptr;
+    size_t size;
+} last_allocated_t;
+
+static last_allocated_t last_allocated[10];
+static int last_allocated_index = 0;
 
 #ifdef HAVE_MEMORY_PROFILING
 #define MP_LOG_PREFIX "==MP "
@@ -29,6 +39,57 @@ bool app_mem_init(void) {
     PRINTF(MP_LOG_PREFIX "init;0x%p;%u\n", buf, buf_size);
 #endif
     return mem_ctx != NULL;
+}
+
+void *app_mem_realloc_impl(void *ptr, size_t size, const char *file, int line) {
+    void *new_ptr;
+    int index;
+    bool found;
+
+    if (ptr != NULL) {
+        found = false;
+        index = last_allocated_index;
+
+        // Look up the last allocated pointer and size
+        for (size_t cnt = 0; cnt < sizeof(last_allocated) / sizeof(last_allocated[0]); ++cnt) {
+            if (last_allocated[index].ptr == ptr) {
+                found = true;
+                break;
+            }
+            index--;
+            if (index < 0) {
+                index = sizeof(last_allocated) / sizeof(last_allocated[0]) - 1;
+            }
+        }
+        LEDGER_ASSERT(found, "Pointer not found in last allocated list");
+
+        // Reallocate memory
+        new_ptr = mem_alloc(mem_ctx, size);
+        memcpy(new_ptr, ptr, last_allocated[index].size);
+        mem_free(mem_ctx, ptr);
+
+        // Clear the cached pointer
+        last_allocated[index].ptr = NULL;
+        last_allocated[index].size = 0;
+    } else {
+        new_ptr = mem_alloc(mem_ctx, size);
+    }
+
+    // Cache the last allocated pointer and size
+    if (new_ptr != NULL) {
+        last_allocated[last_allocated_index].ptr = new_ptr;
+        last_allocated[last_allocated_index].size = size;
+        last_allocated_index++;
+        last_allocated_index %= sizeof(last_allocated) / sizeof(last_allocated[0]);
+    }
+
+#ifdef HAVE_MEMORY_PROFILING
+    PRINTF(MP_LOG_PREFIX "realloc;%u;0x%p;0x%p;%s:%u\n", size, ptr, new_ptr, file, line);
+#else
+    (void) file;
+    (void) line;
+#endif
+    return new_ptr;
 }
 
 void *app_mem_alloc_impl(size_t size, const char *file, int line) {
@@ -50,5 +111,9 @@ void app_mem_free_impl(void *ptr, const char *file, int line) {
     (void) file;
     (void) line;
 #endif
+    if (ptr == NULL) {
+        return;
+    }
+
     mem_free(mem_ctx, ptr);
 }
--- src/mem.h
+++ src/mem.h
@@ -11,9 +11,15 @@
 #define MP_FILE NULL
 #define MP_LINE 0
 #endif
-#define app_mem_alloc(size) app_mem_alloc_impl(size, MP_FILE, MP_LINE)
-#define app_mem_free(ptr)   app_mem_free_impl(ptr, MP_FILE, MP_LINE)
+
+#define app_mem_alloc(size)   app_mem_alloc_impl(size, MP_FILE, MP_LINE)
+#define app_mem_realloc(size) app_mem_realloc_impl(size, MP_FILE, MP_LINE)
+#define app_mem_free(ptr)     app_mem_free_impl(ptr, MP_FILE, MP_LINE)
+
+#define pb_realloc(ptr, size) app_mem_realloc_impl(ptr, size, MP_FILE, MP_LINE)
+#define pb_free(ptr)          app_mem_free_impl(ptr, MP_FILE, MP_LINE)
 
 bool app_mem_init(void);
 void *app_mem_alloc_impl(size_t size, const char *file, int line);
+void *app_mem_realloc_impl(void *ptr, size_t size, const char *file, int line);
 void app_mem_free_impl(void *ptr, const char *file, int line);
