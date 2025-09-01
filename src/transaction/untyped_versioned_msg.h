99820f6cae0c31eab5d7a0a68d1feed6a0d66878
Alexis Grojean <alexis.grojean@ledger.fr>
2025-09-01 17:32:09 +0200

    Add untyped versioned message signing (for topology / onboarding)

---
+++ src/transaction/untyped_versioned_msg.h
@@ -0,0 +1,33 @@
+/*****************************************************************************
+ *   Ledger App Boilerplate.
+ *   (c) 2020 Ledger SAS.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *****************************************************************************/
+
+ #include <stdint.h>   // uint*_t
+ #include <stddef.h>   // size_t
+ #include <stdbool.h>  // bool
+ #include <string.h>   // memmove
+ 
+ #include "os.h"
+ #include "cx.h"
+ #include "ledger_assert.h"
+ 
+ #include "party_id.h"
+ #include "utils.h"
+ 
+ #include "tx_types.h"
+
+ void process_untyped_versioned_msg_tx_init(void);
+int process_untyped_versioned_msg_tx(buffer_t *buf);
