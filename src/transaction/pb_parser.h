efe1cd438e344f404d89f57adffc4c0e77666294
Roman Maslennikov <kafkapro@gmail.com>
2025-10-08 11:11:12 +0300

    Remove unused functions

--- src/transaction/pb_parser.h
+++ src/transaction/pb_parser.h
@@ -82,7 +82,3 @@ void release_node(transaction_ctx_t *tx_ctx);
  *
  */
 void release_metadata(transaction_ctx_t *tx_ctx);
-
-parser_status_e proto_deserialize_input_contract(buffer_t *buf, transaction_ctx_t *tx_ctx);
-
-void release_input_contract(transaction_ctx_t *tx_ctx);
